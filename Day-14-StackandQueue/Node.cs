40f4dc411684ba1d91a33da07d8bdd703cdf4804
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-08 17:26:12 +0530

    [Ronit] Added : Welcome Massage

---
+++ Day-14-StackandQueue/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_StackandQueue
+{
+    internal class Node
+    {
+    }
+}
---
+++ Day-14-StackandQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_StackandQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue");
+        }
+    }
+}
